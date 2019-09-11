79446e70c5eeda2ddffc5267bf56cc37bb9f7d8e
Rebecca Barrett <barrett4467@gmail.com>
2019-09-10 19:05:07 -0600

    Added initial folders and files

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// global vars
+var targetScore = " ";
+var roundScore = " ";
+var wins = " ";
+var losses = " ";
+var crystals = [
+"crystal1", value]
+
+functions
+    function generateRandomNumber() {}
+    function startGame(){}
+    function gameReset(){}
+    function ifWon(){}
+
+// jQuery click events
+// $("crystal1").on("click", function(){})
+// $("crystal2").on("click", function(){})
+// $("crystal3").on("click", function(){})
+// $("crystal4").on("click", function(){})
+
+// use the this to select onclick for crystals 
+
+// crystals will be the same put same class data-value=randomValueOfCrystal find value of each crystal  -->
+
+// <!-- $(".crystal").on("click", function(){
+//     alert("clicked!!");
+// })
